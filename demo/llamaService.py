4f767dcf4e7170689daa3f1e9898e6b3ef7796fb
Shiwan <shiwanzhang6@gmail.com>
2024-05-04 21:21:14 +0200

    llama service sin API key

---
+++ demo/llamaService.py
@@ -0,0 +1,48 @@
+from llama_index import SimpleDirectoryReader, StorageContext, ServiceContext
+from llama_index.indices.vector_store import VectorStoreIndex
+from llama_iris import IRISVectorStore
+
+import getpass
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+# if not os.environ.get("OPENAI_API_KEY"):
+#     os.environ["OPENAI_API_KEY"] = getpass.getpass("")
+
+documents = SimpleDirectoryReader("../data/paul_graham").load_data()
+print("Document ID:", documents[0].doc_id)
+
+username = 'demo'
+password = 'demo' 
+hostname = os.getenv('IRIS_HOSTNAME', 'localhost')
+port = '1972' 
+namespace = 'USER'
+CONNECTION_STRING = f"iris://{username}:{password}@{hostname}:{port}/{namespace}"
+
+vector_store = IRISVectorStore.from_params(
+    connection_string=CONNECTION_STRING,
+    table_name="job_description",
+    embed_dim=1536,  # openai embedding dimension
+)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+# service_context = ServiceContext.from_defaults(
+#     embed_model=embed_model, llm=None
+# )
+
+index = VectorStoreIndex.from_documents(
+    documents, 
+    storage_context=storage_context, 
+    show_progress=True, 
+    # service_context=service_context,
+)
+query_engine = index.as_query_engine()
+
+response = query_engine.query("What did the author do?")
+
+import textwrap
+print(textwrap.fill(str(response), 100))
+
+response = query_engine.query("What happened in the mid 1980s?")
+print(textwrap.fill(str(response), 100))
